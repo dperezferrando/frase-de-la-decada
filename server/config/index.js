17f1ec961522456de7cbb35c2aae06be189950b3
dperezferrando <diego@parsimotion.com>
2020-01-26 18:18:50 -0300

    Update config

--- server/config/index.js
+++ server/config/index.js
@@ -2,6 +2,17 @@ const _ = require("lodash");
 
 const commonConfig = {
  
+  domain: process.env.DOMAIN || "http://localhost:9001",
+
+  google: {
+    clientID: process.env.GOOGLE_OAUTH_CLIENTID,
+    secret: process.env.GOOGLE_OAUTH_SECRET
+  },
+
+  session: {
+    cookieSecret: process.env.COOKIE_SECRET || "a nisman lo mataron"
+  },
+
   mongo: {
     options: {
       useNewUrlParser: true,
