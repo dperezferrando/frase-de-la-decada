5eb7d36b090973841d6745a9052c7abf01fa631c
dperezferrando <diego@parsimotion.com>
2020-01-27 23:06:52 -0300

    Principio de drag and drop

--- client/components/qualifiers/qualifiers.jsx
+++ client/components/qualifiers/qualifiers.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Nav, Navbar } from "react-bootstrap";
 import Component from "../../utils/component"
+import PhrasesDragAndDrop from "./phrasesDragAndDrop";
 
 class Qualifiers extends Component {
   render() {
-    return <div> Qualifiers</div>
+    return <PhrasesDragAndDrop {...this.props}/>
  
   }
 
