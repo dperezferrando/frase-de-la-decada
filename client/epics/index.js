f2cc74a709fbded2930d57709d69743c4b093b4e
dperezferrando <diego@parsimotion.com>
2020-01-27 21:47:42 -0300

    Profile epics

--- client/epics/index.js
+++ client/epics/index.js
@@ -2,10 +2,12 @@ import { combineEpics } from 'redux-observable';
 import _ from "lodash";
 import { catchError } from 'rxjs/operators';
 import exampleEpic from "./example";
+import profileEpic from "./profile";
 
 export default (action$, store$, dependencies) =>
   combineEpics(
-    ...(_.values(exampleEpic))
+    ...(_.values(exampleEpic)),
+    ...(_.values(profileEpic))
   )(action$, store$, dependencies).pipe(
     catchError((error, source) => {
       console.error(error);
