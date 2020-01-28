e43af4d4c23eb88e2812b81fc7dc5ec96c8d849d
dperezferrando <diego@parsimotion.com>
2020-01-27 21:49:11 -0300

    Dibujo la navbar

--- client/components/routes.jsx
+++ client/components/routes.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import Component from "../utils/component";
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
 import ExampleContainer from "../containers/example";
+import Navbar from "../containers/navbar";
 
 const HolaMundo = () => <h1>Hola Mundo</h1>;  
-
 
 class Routes extends Component {
   render(){
     return <BrowserRouter>
+      <Navbar/>
       <Switch>
         <Route exact path="/inicio" component={HolaMundo}/>
         <Route exact path="/test" component={ExampleContainer}/>
