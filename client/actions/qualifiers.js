3815dc574d43a48fa14b9dd7f4da54b5488c6319
dperezferrando <diego@parsimotion.com>
2020-01-28 22:45:10 -0300

    Agrego fetch y set de frases

--- client/actions/qualifiers.js
+++ client/actions/qualifiers.js
@@ -1,9 +1,17 @@
 export const Types = {
-
+  FETCH_FRASES:"FETCH_FRASES",
+  SET_FRASES: "SET_FRASES"
 }
 
 
 export const actions = {
-
+  fetchFrases: (options) => ({
+    type: Types.FETCH_FRASES,
+    options
+  }),
+  setFrases: (frases) => ({
+    type: Types.SET_FRASES,
+    frases
+  })
 
 }
