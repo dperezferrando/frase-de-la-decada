fdcb97c1e212aa2208b6934abe4169e7fad6328c
dperezferrando <diego@parsimotion.com>
2020-01-28 22:08:21 -0300

    Agrego routes de frases

--- server/routes/setUpApiRoutes.js
+++ server/routes/setUpApiRoutes.js
@@ -1,9 +1,11 @@
 import users from "../api/user";
+import frases from "../api/frase";
 
 export default (app) => {
 
   app.use("/api/test", ({ user }, res) => res.send({ test: `hola ${user.name}`}));
   app.use("/api/users", users);
+  app.use("/api/frases", frases);
 
 
 }
