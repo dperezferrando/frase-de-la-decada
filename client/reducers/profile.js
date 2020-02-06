23d60de2840be7e965a51eb212efb2d776b54dd4
dperezferrando <diego@parsimotion.com>
2020-02-05 22:35:08 -0300

    Default user - a ver si se va el bug

--- client/reducers/profile.js
+++ client/reducers/profile.js
@@ -1,6 +1,6 @@
 import { Types } from "../actions/profile";
 
- export default (state = { user: {}, isLoading: true }, action) => {
+ export default (state = { user: { voted: {} }, isLoading: true }, action) => {
     switch(action.type) {
       case Types.FETCH_USER:
         return {...state, isLoading: true }
