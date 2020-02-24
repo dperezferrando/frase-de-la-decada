96d3e5fcaacce56000ef91112190f9ea144b0223
dperezferrando <diego@parsimotion.com>
2020-02-24 17:17:43 -0300

    Job listo

--- server/jobs/groupDraw/groupDraw.js
+++ server/jobs/groupDraw/groupDraw.js
@@ -1,6 +1,9 @@
 import FraseHome from "./domain/homes/frase.home.js";
 import mongoose from "mongoose";
 import _ from "lodash";
+import Promise from "bluebird";
+
+const GROUPS = ["A", "B", "C", "D", "E", "F", "G", "H"]
 
 const config = require("./config")
 console.log(config.mongo.uri)
@@ -25,15 +28,30 @@ const fillGroups = (groups, bombos) => {
 
 const createBombos = (qualified) => _.chunk(qualified, 8);
 
+
+const validGroup = (group) => {
+  const byAnio = _.countBy(group, "anio");
+  const byAutor = _.countBy(group, "autor");
+
+  return _.every(byAnio, value => value <= 2) && _.every(byAutor, value => value <= 2)
+}
+
+const validateDraw = (groups) => {
+  return _.every(groups, validGroup)
+}
+
 const makeDraw = bombos => {
   const first = _.first(bombos);
   const groups = first.map(it => [it]);
   fillGroups(groups, _.drop(bombos, 1));
-  return groups;
+  return validateDraw(groups) ? groups : makeDraw(bombos);
 
 }
 
 
+const persistGroup = (group, i) => {
+  return Promise.map(group, frase =>  fraseHome.update({ _id: frase._id }, { group: GROUPS[i] }))
+}
 
 
 fraseHome.aggregate([
@@ -44,6 +62,8 @@ fraseHome.aggregate([
 .then(qualified => createBombos(qualified))
 .then(bombos => makeDraw(bombos))
 .tap(groups => console.log(JSON.stringify(groups)))
+.map((group, i) => persistGroup(group, i))
+.then(() => process.exit(0))
 
 
 
