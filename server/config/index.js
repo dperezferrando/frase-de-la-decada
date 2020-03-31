320d0ee0253939f59923a01b00b826d9d97140b5
dperezferrando <diego@parsimotion.com>
2020-03-31 14:01:40 -0300

    Config

--- server/config/index.js
+++ server/config/index.js
@@ -24,6 +24,27 @@ const commonConfig = {
     endDate: moment(new Date(process.env.GROUPSTAGE_END_DATE || "1/1/2021"))
   },
 
+  eigthsStage: {
+    startDate: moment(new Date(process.env.EIGHTSTAGE_START_DATE || "1/1/2020")),
+    endDate: moment(new Date(process.env.EIGHTSTAGE_END_DATE || "1/1/2021"))
+  },
+
+  fourthsStage: {
+    startDate: moment(new Date(process.env.FOURTHSTAGE_START_DATE || "1/1/2020")),
+    endDate: moment(new Date(process.env.FOURTHSTAGE_END_DATE || "1/1/2021"))
+  },
+
+  semiStage: {
+    startDate: moment(new Date(process.env.SEMISTAGE_START_DATE || "1/1/2020")),
+    endDate: moment(new Date(process.env.SEMISTAGE_END_DATE || "1/1/2021"))
+  },
+  
+  finalStage: {
+    startDate: moment(new Date(process.env.FINALSTAGE_START_DATE || "1/1/2020")),
+    endDate: moment(new Date(process.env.FINALSTAGE_END_DATE || "1/1/2021"))
+  },
+
+
   mongo: {
     options: {
       useNewUrlParser: true,
