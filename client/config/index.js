852e98888dda859846593dad032ee75012756de1
dperezferrando <diego@parsimotion.com>
2020-03-31 14:03:45 -0300

    Client config

--- client/config/index.js
+++ client/config/index.js
@@ -12,6 +12,26 @@ module.exports = {
     startDate: moment(new Date(process.env.GROUPSTAGE_START_DATE || "1/1/2020")),
     endDate: moment(new Date(process.env.GROUPSTAGE_END_DATE || "1/1/2021")),
     resultsDate: moment(new Date(process.env.GROUPSTAGE_RESULTS_DATE || "1/1/2021"))
-  }
+  },
+  eightsStage: {
+    startDate: moment(new Date(process.env.EIGHTSTAGE_START_DATE || "1/1/2020")),
+    endDate: moment(new Date(process.env.EIGHTSTAGE_END_DATE || "1/1/2021"))
+  },
+
+  fourthsStage: {
+    startDate: moment(new Date(process.env.FOURTHSTAGE_START_DATE || "1/1/2020")),
+    endDate: moment(new Date(process.env.FOURTHSTAGE_END_DATE || "1/1/2021"))
+  },
+
+  semiStage: {
+    startDate: moment(new Date(process.env.SEMISTAGE_START_DATE || "1/1/2020")),
+    endDate: moment(new Date(process.env.SEMISTAGE_END_DATE || "1/1/2021"))
+  },
+  
+  finalStage: {
+    startDate: moment(new Date(process.env.FINALSTAGE_START_DATE || "1/1/2020")),
+    endDate: moment(new Date(process.env.FINALSTAGE_END_DATE || "1/1/2021"))
+  },
+
 
 }
