b2bee88d3643a3a05d2570e32286f64e5501df22
dperezferrando <diego@parsimotion.com>
2020-05-03 23:00:15 -0300

    Update config

--- server/config/index.js
+++ server/config/index.js
@@ -48,6 +48,12 @@ const commonConfig = {
     resultsDate: moment(new Date(process.env.FINAL_RESULTS_DATE || "1/1/2021"))
   },
 
+  thirdPlace: {
+    startDate: moment(new Date(process.env.THIRDPLACE_START_DATE || "1/1/2020")),
+    endDate: moment(new Date(process.env.THIRDPLACE_END_DATE || "1/1/2021")),
+    resultsDate: moment(new Date(process.env.THIRDPLACE_RESULTS_DATE || "1/1/2021"))
+  },
+
 
   mongo: {
     options: {
