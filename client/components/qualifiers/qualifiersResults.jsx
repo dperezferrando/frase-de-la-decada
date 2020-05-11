970c922198865f56e1a58f68e7f879e2e171f9c0
dperezferrando <diego@parsimotion.com>
2020-05-10 21:14:31 -0300

    Se colapsan resultados

--- client/components/qualifiers/qualifiersResults.jsx
+++ client/components/qualifiers/qualifiersResults.jsx
@@ -1,38 +1,57 @@
 import React from 'react';
 import Component from "../../utils/component"
-import { Col, Row , Table} from "react-bootstrap";
+import { Col, Row , Table, Collapse } from "react-bootstrap";
 import "./qualifiers.css"
 
 class QualifiersResults extends Component {
+
+  state = {
+    collapse: false
+  }
+
   render() {
+    const chevron = this.state.collapse ? "right" : "down";
     return <Row className="justify-content-md-center" style={{ marginTop: "10px"}}>
       <Col md={11}>
-        <h4><b>Frases clasificadas:</b></h4>
-        <Table striped bordered>
-          <thead>
-            <tr>
-              <th>#</th>
-              <th>Frase</th>
-              <th>Votos</th>
-              <th>Coeficiente Autista</th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.props.frases.results.map((frase, i) => {
+        <div className="clasificadas">
+          <div className="chevron" onClick={::this.collapse} aria-controls="tableQualifiers" aria-expanded={!this.state.collapse}>
+            <i className={`fa fa-chevron-${chevron}`} aria-hidden='true'/>
+          </div>
+          <div className="resultsTitle">Frases clasificadas</div>
+            <div className="tableQualifiers">
+            <Collapse in={!this.state.collapse}>
+              <Table striped bordered>
+                <thead>
+                  <tr>
+                    <th>#</th>
+                    <th>Frase</th>
+                    <th>Votos</th>
+                    <th>Coeficiente Autista</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {this.props.frases.results.map((frase, i) => {
 
-              return <tr key={`fraseclasif-${i}`} className={frase.qualified? "qualified" : ""}>
-                <td className="yearAuthor">{i + 1}</td>
-                <td><span>{`${frase.frase} - `}</span><span className="yearAuthor">{`${frase.autor} (${frase.anio})` }</span></td>
-                <td className="yearAuthor">{frase.votesQuantity.qualifiers}</td>
-                <td className="yearAuthor">{frase.coeficienteAutista}</td>
-              </tr>
+                    return <tr key={`fraseclasif-${i}`} className={frase.qualified? "qualified" : ""}>
+                      <td className="yearAuthor">{i + 1}</td>
+                      <td><span>{`${frase.frase} - `}</span><span className="yearAuthor">{`${frase.autor} (${frase.anio})` }</span></td>
+                      <td className="yearAuthor">{frase.votesQuantity.qualifiers}</td>
+                      <td className="yearAuthor">{frase.coeficienteAutista}</td>
+                    </tr>
 
 
-            })}
-          </tbody>
-        </Table>
+                  })}
+                </tbody>
+              </Table>
+            </Collapse>
+          </div>
+        </div>
       </Col>
     </Row>
+  }
+
+  collapse() {
+    this.setState({ ...this.state,  collapse: !this.state.collapse })
   }
 
 }
