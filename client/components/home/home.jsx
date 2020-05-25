6cd10fc89c064b7a8e2e9c4906f5f3ea711d7837
dperezferrando <diego@parsimotion.com>
2020-05-25 19:06:02 -0300

    Principio ui news

--- client/components/home/home.jsx
+++ client/components/home/home.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import { Container, Row, Col, Form } from "react-bootstrap";
 import Component from "../../utils/component"
+import ReactMarkdown from "react-markdown";
+import "./home.css";
 
 class Home extends Component {
-
   render() {
-    return <h1>Hola Mundo</h1>;
+  const input = '# Hola \n\n *aaaa*'
+    return <Row className="justify-content-md-center groupStage">
+      <Col md={11}>
+        <div className="newsContainer">
+          <ReactMarkdown source={input}/>
+        </div>
+      </Col>
+    </Row>
   }
 
 
