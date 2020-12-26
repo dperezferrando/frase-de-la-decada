922d844187be4447a1f2bb9c604a6a700b2fc2fc
Diego <diegoperezferrando@gmail.com>
2020-12-26 17:07:44 -0300

    Se ven las stats

--- client/components/resultados/resultados.jsx
+++ client/components/resultados/resultados.jsx
@@ -7,6 +7,21 @@ import config from "../../config";
 import QualifiersResults from "../../containers/qualifiers/qualifiersResults";
 import "./resultados.css"
 
+const getList = (list, text) => {
+  const total = _.sumBy(list, "count");
+  return (
+    <span>
+        <p className="detailAutor">{ text }:</p>
+        <p className="detailFrase"> { total }</p>
+        { 
+          list.map(({ _id, count }) => {
+            return <p><span className="detailAutor">{ _id }</span> - <span className="detailFrase">{ `${count} (${(count/total*100).toFixed(2)}%)` }</span></p>
+          })
+        }
+      </span>
+
+    )
+}
 
 class Resultados extends Component {
 
@@ -14,16 +29,35 @@ class Resultados extends Component {
     return (
         <Row className="justify-content-md-center">
           <Col md={11}>
-          <div className="mainResults">
-            <div className="mainTitle">RESULTADOS 2020</div>
-            <div className="fraseAnio"> 
-              <p className="detailAutor">{this.props.mostVoted.autor}</p>
-              <p className="detailFrase">{this.props.mostVoted.frase} { this.props.mostVoted.aclaracion && `(${this.props.mostVoted.aclaracion})`}</p>
-              <p className="detailAutor">FRASE DEL AÑO</p>
-
+            <div className="mainResults">
+              <Row>
+                <Col md={12}>
+                  <div className="mainTitle">RESULTADOS 2020</div>
+                  <div className="fraseAnio"> 
+                    <p className="detailAutor">{this.props.mostVoted.autor}</p>
+                    <p className="detailFrase">{this.props.mostVoted.frase} { this.props.mostVoted.aclaracion && `(${this.props.mostVoted.aclaracion})`}</p>
+                    <p className="detailAutor">FRASE DEL AÑO</p>
+                  </div>
+                </Col>
+              </Row>
+              <Row>
+                <Col md={12}>
+                  <Row>
+                    <Col md={6}>
+                      {getList(this.props.stats.frasesCount, "CANTIDAD TOTAL DE FRASES")}
+                    </Col>
+                    <Col md={6}>
+                      {getList(_.filter(this.props.stats.votesCount, ({ count }) => count > 0), "CANTIDAD TOTAL DE VOTOS")}
+                    </Col>
+                  </Row>
+                </Col>
+              </Row>
+              <Row>
+                <Col md={12}>
+                  <QualifiersResults/>
+                </Col>
+              </Row>
             </div>
-          <QualifiersResults/>
-          </div>
           </Col>
         </Row>
       
