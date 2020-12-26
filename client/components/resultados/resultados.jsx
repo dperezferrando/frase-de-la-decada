c2966d1e8d7ec8124f95fd665bc0ce47bdb846bc
Diego <diegoperezferrando@gmail.com>
2020-12-26 14:04:15 -0300

    Estilo resultados frase anio

--- client/components/resultados/resultados.jsx
+++ client/components/resultados/resultados.jsx
@@ -4,6 +4,7 @@ import moment from "moment";
 import Component from "../../utils/component"
 import WithLoading from "../../components/utils/withLoading";
 import config from "../../config";
+import "./resultados.css"
 
 
 class Resultados extends Component {
@@ -12,7 +13,15 @@ class Resultados extends Component {
     return (
         <Row className="justify-content-md-center">
           <Col md={11}>
-            hola
+          <div className="mainResults">
+            <div className="mainTitle">RESULTADOS 2020</div>
+            <div className="fraseAnio"> 
+              <p className="detailAutor">Pérez</p>
+              <p className="detailFrase">La concha tuya (Puto)</p>
+              <p className="detailAutor">FRASE DEL AÑO</p>
+
+            </div>
+          </div>
           </Col>
         </Row>
       
